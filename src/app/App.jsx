b97e8274b56842073a02b39c6abfc641980a8da1
Rebeca Gaia <reb.gaia@gmail.com>
2021-01-20 18:15:29 -0300

    create the project struture

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+
+export default props => (
+  <div className="backgroundApp">
+
+    <Header></Header>
+
+    <div className="title">
+      <h1>Hello...</h1>
+      <h2>I'm a Software Developer</h2>
+    </div>
+
+  </div>
+)
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css';
+
+export default props => (
+    <div className='header'>
+        <h3>About</h3> {/* profissional aquisition */}
+        <h3>Academic</h3> {/* schools */}
+        <h3>Experiences</h3> {/* works and apresentations */}
+        <h3>Projects</h3> {/* projects */}
+    </div>
+)
